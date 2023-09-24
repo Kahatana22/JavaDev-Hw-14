d1f72e24728244c3b373b80f13ee56d0fbd0e1c5
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-09-24 17:44:05 +0300

    Create NoteController.java

---
+++ src/main/java/com/example/JavaDevHw14/controllers/NoteController.java
@@ -0,0 +1,55 @@
+package com.example.JavaDevHw14.controllers;
+
+import com.example.JavaDevHw14.entities.Note;
+import com.example.JavaDevHw14.services.NoteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/note")
+public class NoteController {
+    private NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+    @GetMapping(value = "/list")
+    public ModelAndView getListOfNotes() {
+        ModelAndView model = new ModelAndView("note/list");
+        model.addObject("notes", noteService.listAll());
+        return model;
+    }
+
+    @GetMapping(value = "/create")
+    public String createNote() {
+        return "note/note";
+    }
+
+    @PostMapping(path = "/create")
+    public String updateListOfNodes(@ModelAttribute("note") Note newNote) {
+        noteService.add(newNote);
+        return "redirect:/note/list";
+    }
+
+    @PostMapping(path = "/delete")
+    public String deleteNote(@RequestParam("id") Long id) {
+        noteService.deleteById(id);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping(value = "/edit")
+    public ModelAndView editNote(@RequestParam("id") Long id) {
+        ModelAndView edit = new ModelAndView("note/edit");
+        Note note = noteService.getById(id).get();
+        edit.addObject("note", note);
+        return edit;
+    }
+
+    @PostMapping(path = "/edit")
+    public String updateNote(@ModelAttribute("note") Note updateNote) {
+        noteService.update(updateNote);
+        return "redirect:/note/list";
+    }
+}
